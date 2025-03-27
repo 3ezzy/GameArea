33b5a648c3dae728178eb7bfa3dd0ff56585454e
Mehdi <khammali26@gmail.com>
2025-03-27 11:53:18 +0000

    Add mass assignable and hidden attributes to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-
-
 class User extends Authenticatable implements JWTSubject
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     /**
      * Get the identifier that will be stored in the JWT token.
      */
